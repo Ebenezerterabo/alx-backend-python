ab32ab0bcbebe51803ab283a1ad5a611c00bdd38
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-08-24 04:14:25 +0000

    Task 3 solved

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""" 3-tasks.py """
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int = 10) -> float:
+    """ returns a float """
+    return asyncio.create_task(wait_random(max_delay))
