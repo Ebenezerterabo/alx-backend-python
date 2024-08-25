2709b113e4c7399ad74baa44a55b03cf5fba6360
Ebenezerterabo <Ebenezerterabo@gmail.com>
2024-08-25 22:09:02 +0000

    Task 0 solved

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ 0-async_generator.py """
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """" loop 10 times each time asynchronously wait 1 second """
     for _ in range(10):
         await asyncio.sleep(1)
