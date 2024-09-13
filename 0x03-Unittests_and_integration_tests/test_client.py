9e620e71d1a7c9b7bb504c6c5a426afa142a3dd7
Ebenezer terabo <teraboebenezer@gmail.occom>
2024-09-13 22:53:40 +0000

    Task solved

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,12 +2,11 @@
 """Generic test utilities for github org client.
 """
 import unittest
-from unittest.mock import patch, MagicMock
-from parameterized import parameterized
+from unittest.mock import patch, MagicMock, Mock
+from parameterized import parameterized  # type: ignore
 from client import GithubOrgClient
 from typing import (
     Any,
-    Mock
 )
 
 
@@ -25,3 +24,31 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client.org(), expected)
         mock_get_json.assert_called_once_with(
             client.ORG_URL.format(org=org_name))
+
+    def test_public_repos(self, url: str, expected: Any, mock_get_json: Mock):
+        """ Implement the test_public_repos method to unit test
+            the public_repos method of the GithubOrgClient class.
+        """
+        mock_get_json.return_value = MagicMock(return_value=expected)
+        client = GithubOrgClient("org")
+        self.assertEqual(client.public_repos(), expected)
+        mock_get_json.assert_called_once_with(url)
+
+    @patch("client.GithubOrgClient.public_repos")
+    def test_public_repos(self, url: str, expected: Any, mock_get_json: Mock):
+        """ Implement the test_public_repos method to unit test
+            the public_repos method of the GithubOrgClient class.
+        """
+        mock_get_json.return_value = MagicMock(return_value=expected)
+        client = GithubOrgClient("org")
+        self.assertEqual(client.public_repos(), expected)
+        mock_get_json.assert_called_once_with(url)
+
+    def test_has_license(self, url: str, expected: Any, mock_get_json: Mock):
+        """ Implement the test_has_license method to unit test
+            the has_license method of the GithubOrgClient class.
+        """
+        mock_get_json.return_value = MagicMock(return_value=expected)
+        client = GithubOrgClient("org")
+        self.assertEqual(client.has_license(repo=expected), expected)
+        mock_get_json.assert_called_once_with(url)
